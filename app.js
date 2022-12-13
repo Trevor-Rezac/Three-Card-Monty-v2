8abf64f4dbd1e746ccacc68ab9967da33dd15a69
Trevor Rezac <trev.rezac@gmail.com>
2022-12-12 22:56:24 -0800

    game logic is completed, working on shuffle animations

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
+const cardContainer = document.querySelector(".card-container");
 const cards = document.querySelectorAll(".card");
 
-const correctCards = ["left", "middle", "right"];
+const faceDownCards = ["left", "middle", "right"];
+const aces = ["AoS", "AoD", "AoC", "AoH"];
+const jokers = ["Joker1", "Joker2", "Joker3"];
 
 cards.forEach((card) => {
   card.addEventListener("click", () => {
-    const correctCard = getRandomItem(correctCards);
+    const correctCard = getRandomItem(faceDownCards);
     handleGuess(card, correctCard);
   });
 });
@@ -15,12 +18,51 @@ function getRandomItem(arr) {
 }
 
 function handleGuess(userGuess, correctCard) {
-  console.log("userGuess", userGuess);
-  console.log("correctCard", correctCard);
+  resetCards();
+
+  const userGuessImg = userGuess.getElementsByTagName("img")[0];
+  const randomAce = getRandomItem(aces);
+  const randomJoker = getRandomItem(jokers);
 
   if (userGuess.classList.contains(correctCard)) {
-    console.log("correct!");
+    userGuessImg.src = `./assets/requiem${randomAce}.jpeg`;
   } else {
-    console.log("wrong");
+    userGuessImg.src = `./assets/requiem${randomJoker}.jpeg`;
   }
+
+  disablePointer();
+  setTimeout(shuffleCards, 2000);
+  newHand();
+}
+
+function shuffleCards() {
+  resetCards();
+
+  cards[0].style.animation = "shuffleCard1 1s alternate ease-in-out 3";
+  cards[1].style.animation = "shuffleCard2 1s alternate ease-in-out 3";
+  cards[2].style.animation = "shuffleCard3 1s alternate ease-in-out 3";
+}
+
+function newHand() {
+  cards.forEach((card) => {
+    card.style.animation = "";
+  });
+  enablePointer();
+  clearTimeout(shuffleCards);
+}
+
+function resetCards() {
+  cards.forEach((card) => {
+    card.getElementsByTagName("img")[0].src = "./assets/requiemBack.jpeg";
+  });
+}
+
+function disablePointer() {
+  const cardContainer = document.querySelector(".card-container");
+  cardContainer.classList.add("disablePointer");
+}
+
+function enablePointer() {
+  const cardContainer = document.querySelector(".card-container");
+  cardContainer.classList.remove("disablePointer");
 }
