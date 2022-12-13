e8bf5b56ac62638d93cc10a6f383df224176ece7
Trevor Rezac <trev.rezac@gmail.com>
2022-12-12 17:39:40 -0800

    basic workings of a guessing game in js

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
+const cards = document.querySelectorAll(".card");
 
+const correctCards = ["left", "middle", "right"];
+
+cards.forEach((card) => {
+  card.addEventListener("click", () => {
+    const correctCard = getRandomItem(correctCards);
+    handleGuess(card, correctCard);
+  });
+});
+
+function getRandomItem(arr) {
+  const i = Math.floor(Math.random() * arr.length);
+  return arr[i];
+}
+
+function handleGuess(userGuess, correctCard) {
+  console.log("userGuess", userGuess);
+  console.log("correctCard", correctCard);
+
+  if (userGuess.classList.contains(correctCard)) {
+    console.log("correct!");
+  } else {
+    console.log("wrong");
+  }
+}
